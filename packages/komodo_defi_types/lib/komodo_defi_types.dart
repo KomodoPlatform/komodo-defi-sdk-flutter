f4edd33bcf9a6729d38166f31bd239b341edc291
CharlVS <77973576+CharlVS@users.noreply.github.com>
2025-01-17 17:07:29 +0100

    fix(types): Make types index private

--- packages/komodo_defi_types/lib/komodo_defi_types.dart
+++ packages/komodo_defi_types/lib/komodo_defi_types.dart
@@ -16,7 +16,7 @@ export 'src/auth/kdf_user.dart';
 // Aliased/proxied types
 export 'src/exported_rpc_types.dart';
 export 'src/komodo_defi_types_base.dart';
-export 'types.dart';
+export 'src/types.dart';
 
 // Export activation params types
 // export 'packages:komodo_defi_rpc_methods/lib/src/common_structures/activation/activation_params/activation_params_index.dart
