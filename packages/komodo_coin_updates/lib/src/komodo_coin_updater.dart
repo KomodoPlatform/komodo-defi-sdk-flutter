8e4d525c508594b37131f6ea6bba3f623e4ea4a2
Francois <takenagain@users.noreply.github.com>
2025-03-31 13:38:00 +0200

    chore(deps): update dependencies and add very_good_analysis

--- packages/komodo_coin_updates/lib/src/komodo_coin_updater.dart
+++ packages/komodo_coin_updates/lib/src/komodo_coin_updater.dart
@@ -1,7 +1,5 @@
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:komodo_coin_updates/src/persistence/hive/hive.dart';
 import 'package:komodo_coin_updates/src/persistence/persisted_types.dart';
-import 'package:komodo_coin_updates/src/persistence/persistence_provider.dart';
 
 import 'models/coin_info.dart';
 import 'models/models.dart';
