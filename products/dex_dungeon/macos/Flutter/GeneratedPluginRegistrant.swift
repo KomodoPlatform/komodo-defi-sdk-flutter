fd2737e82eb357eaf46096048d14066ec4e0e5d4
CharlVS <77973576+CharlVS@users.noreply.github.com>
2025-05-24 15:42:08 +0200

    chore(deps): Upgrade packages

--- products/dex_dungeon/macos/Flutter/GeneratedPluginRegistrant.swift
+++ products/dex_dungeon/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import flutter_secure_storage_darwin
+import local_auth_darwin
+import mobile_scanner
 import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
