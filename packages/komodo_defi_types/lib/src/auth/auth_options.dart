3fab8c3dc985201ef0aaca27b6db242fdc8a665c
Francois <takenagain@users.noreply.github.com>
2025-06-05 15:41:06 +0200

    fix(trezor,activation): add PrivateKeyPolicy to AuthOptions and beyond

--- packages/komodo_defi_types/lib/src/auth/auth_options.dart
+++ packages/komodo_defi_types/lib/src/auth/auth_options.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:komodo_defi_rpc_methods/komodo_defi_rpc_methods.dart';
 import 'package:komodo_defi_types/komodo_defi_type_utils.dart';
 import 'package:komodo_defi_types/komodo_defi_types.dart';
 
@@ -6,6 +7,7 @@ class AuthOptions extends Equatable {
   const AuthOptions({
     required this.derivationMethod,
     this.allowWeakPassword = false,
+    this.privKeyPolicy = PrivateKeyPolicy.contextPrivKey,
   });
 
   factory AuthOptions.fromJson(JsonMap json) {
@@ -13,19 +15,25 @@ class AuthOptions extends Equatable {
       derivationMethod:
           DerivationMethod.parse(json.value<String>('derivation_method')),
       allowWeakPassword: json.valueOrNull<bool>('allow_weak_password') ?? false,
+      privKeyPolicy: json.valueOrNull<String>('priv_key_policy') == 'Trezor'
+          ? PrivateKeyPolicy.trezor
+          : PrivateKeyPolicy.contextPrivKey,
     );
   }
 
   final DerivationMethod derivationMethod;
   final bool allowWeakPassword;
+  final PrivateKeyPolicy privKeyPolicy;
 
   JsonMap toJson() {
     return {
       'derivation_method': derivationMethod.toString(),
       'allow_weak_password': allowWeakPassword,
+      'priv_key_policy': privKeyPolicy.id,
     };
   }
 
   @override
-  List<Object?> get props => [derivationMethod, allowWeakPassword];
+  List<Object?> get props =>
+      [derivationMethod, allowWeakPassword, privKeyPolicy];
 }
