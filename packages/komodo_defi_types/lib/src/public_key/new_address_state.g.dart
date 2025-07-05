d7b6edef4521fd64b957e7020c48452d2c3676da
Francois <takenagain@users.noreply.github.com>
2025-07-05 18:37:15 +0200

    feat(pubkey): add streamed new address API with Trezor confirmations (#123)
    * feat(pubkey): add streamed new address API with Trezor confirmations (#117)
    
    * Implement streamed new address state with confirmations
    
    * refactor: remove unused imports and make polling interval configurable
    
    * refactor: move confirm address details to types (#120)
    
    * refactor: move confirm address details to types
    
    * refactor: fix warnings
    
    * feat(example): use streamed address creation (#119)
    
    * Use streamed address generation
    
    * refactor: add cancel action and use normal stream rather than broadcast
    
    * chore: add json conversion support to `NewAddressState`
    
    * fix(asset-filter): exclude BCH and yield address creation error states
    
    * refactor: add task description parser and try cancel task if user cancel

---
+++ packages/komodo_defi_types/lib/src/public_key/new_address_state.g.dart
@@ -0,0 +1,42 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'new_address_state.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_NewAddressState _$NewAddressStateFromJson(Map<String, dynamic> json) =>
+    _NewAddressState(
+      status: $enumDecode(_$NewAddressStatusEnumMap, json['status']),
+      message: json['message'] as String?,
+      taskId: (json['taskId'] as num?)?.toInt(),
+      address: json['address'] == null
+          ? null
+          : NewAddressInfo.fromJson(json['address'] as Map<String, dynamic>),
+      expectedAddress: json['expectedAddress'] as String?,
+      error: json['error'] as String?,
+    );
+
+Map<String, dynamic> _$NewAddressStateToJson(_NewAddressState instance) =>
+    <String, dynamic>{
+      'status': _$NewAddressStatusEnumMap[instance.status]!,
+      'message': instance.message,
+      'taskId': instance.taskId,
+      'address': instance.address,
+      'expectedAddress': instance.expectedAddress,
+      'error': instance.error,
+    };
+
+const _$NewAddressStatusEnumMap = {
+  NewAddressStatus.initializing: 'initializing',
+  NewAddressStatus.waitingForDevice: 'waitingForDevice',
+  NewAddressStatus.waitingForDeviceConfirmation: 'waitingForDeviceConfirmation',
+  NewAddressStatus.pinRequired: 'pinRequired',
+  NewAddressStatus.passphraseRequired: 'passphraseRequired',
+  NewAddressStatus.confirmAddress: 'confirmAddress',
+  NewAddressStatus.processing: 'processing',
+  NewAddressStatus.completed: 'completed',
+  NewAddressStatus.error: 'error',
+  NewAddressStatus.cancelled: 'cancelled',
+};
