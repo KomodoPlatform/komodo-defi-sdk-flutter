158673d0731ff3718c9120cd96766342877e8595
Francois <takenagain@users.noreply.github.com>
2025-07-18 11:10:15 +0200

    Add MigrationPreview model

---
+++ packages/komodo_defi_types/lib/src/migration/migration_preview.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:komodo_defi_types/komodo_defi_type_utils.dart';
+import 'package:komodo_defi_types/komodo_defi_types.dart'
+    show WithdrawResult, WalletId, WithdrawalPreview;
+
+part 'migration_preview.freezed.dart';
+part 'migration_preview.g.dart';
+
+@freezed
+abstract class MigrationPreview with _$MigrationPreview {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory MigrationPreview({
+    required WalletId fromWalletId,
+    required WalletId toWalletId,
+    required String pubkeyHash,
+    required List<WithdrawalPreview> withdrawals,
+  }) = _MigrationPreview;
+
+  factory MigrationPreview.fromJson(JsonMap json) =>
+      _$MigrationPreviewFromJson(json);
+}
---
+++ packages/komodo_defi_types/lib/src/migration/migration_preview.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'migration_preview.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_MigrationPreview _$MigrationPreviewFromJson(Map<String, dynamic> json) =>
+    _MigrationPreview(
+      fromWalletId:
+          WalletId.fromJson(json['from_wallet_id'] as Map<String, dynamic>),
+      toWalletId:
+          WalletId.fromJson(json['to_wallet_id'] as Map<String, dynamic>),
+      pubkeyHash: json['pubkey_hash'] as String,
+      withdrawals: (json['withdrawals'] as List<dynamic>)
+          .map((e) => WithdrawResult.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$MigrationPreviewToJson(_MigrationPreview instance) =>
+    <String, dynamic>{
+      'from_wallet_id': instance.fromWalletId,
+      'to_wallet_id': instance.toWalletId,
+      'pubkey_hash': instance.pubkeyHash,
+      'withdrawals': instance.withdrawals,
+    };
