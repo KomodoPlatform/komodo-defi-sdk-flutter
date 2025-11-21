8b0cedb5f2f8b1adea727e4edf564a419c61ea18
Francois <takenagain@users.noreply.github.com>
2025-11-21 20:09:03 +0200

    refactor(activation): add logging to SIA activation strategy

--- packages/komodo_defi_sdk/lib/src/activation/protocol_strategies/sia_activation_strategy.dart
+++ packages/komodo_defi_sdk/lib/src/activation/protocol_strategies/sia_activation_strategy.dart
@@ -1,13 +1,17 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:komodo_defi_framework/komodo_defi_framework.dart';
 import 'package:komodo_defi_rpc_methods/komodo_defi_rpc_methods.dart';
 import 'package:komodo_defi_sdk/src/activation/_activation.dart';
 import 'package:komodo_defi_types/komodo_defi_types.dart';
+import 'package:logging/logging.dart';
 
 class SiaActivationStrategy extends ProtocolActivationStrategy {
   SiaActivationStrategy(super.client);
 
   static const Duration kPollInterval = Duration(milliseconds: 500);
+  static final Logger _log = Logger('SiaActivationStrategy');
 
   @override
   Set<CoinSubClass> get supportedProtocols => {CoinSubClass.sia};
@@ -32,25 +36,35 @@ class SiaActivationStrategy extends ProtocolActivationStrategy {
       requiredConfirmations: protocol.requiredConfirmations,
     );
 
-    yield ActivationProgress(
+    yield const ActivationProgress(
       status: 'Starting SIA activation...',
       progressDetails: ActivationProgressDetails(
         currentStep: ActivationStep.initialization,
         stepCount: 3,
-        additionalInfo: {
-          'assetType': 'platform',
-          'protocol': 'SIA',
-        },
+        additionalInfo: {'assetType': 'platform', 'protocol': 'SIA'},
       ),
     );
 
     try {
-      final init = await KomodoDefiRpcMethods(client).sia.enableSiaInit(
-            ticker: asset.id.id,
-            params: params,
+      // Debug logging for SIA activation
+      if (KdfLoggingConfig.verboseLogging) {
+        _log
+          ..info('[SIA] Activating SIA coin: ${asset.id.id}')
+          ..info(
+            '[SIA] Activation parameters: ${jsonEncode({'ticker': asset.id.id, 'server_url': serverUrl, 'required_confirmations': protocol.requiredConfirmations})}',
           );
+      }
+
+      final init = await KomodoDefiRpcMethods(
+        client,
+      ).sia.enableSiaInit(ticker: asset.id.id, params: params);
 
       final taskId = init.taskId;
+
+      if (KdfLoggingConfig.verboseLogging) {
+        _log.info('[SIA] Task initiated for ${asset.id.id}, task_id: $taskId');
+      }
+
       yield ActivationProgress(
         status: 'SIA activation task started',
         progressDetails: ActivationProgressDetails(
@@ -61,8 +75,9 @@ class SiaActivationStrategy extends ProtocolActivationStrategy {
       );
 
       while (true) {
-        final status =
-            await KomodoDefiRpcMethods(client).sia.enableSiaStatus(taskId);
+        final status = await KomodoDefiRpcMethods(
+          client,
+        ).sia.enableSiaStatus(taskId);
 
         yield ActivationProgress(
           status: 'SIA activation in progress',
@@ -76,6 +91,10 @@ class SiaActivationStrategy extends ProtocolActivationStrategy {
         // Stop polling on any terminal state
         if (status.status != 'InProgress') {
           if (status.status == 'Ok') {
+            if (KdfLoggingConfig.verboseLogging) {
+              _log.info('[SIA] Activation completed for ${asset.id.id}');
+            }
+
             yield ActivationProgress(
               status: 'SIA activation complete',
               isComplete: true,
@@ -86,6 +105,12 @@ class SiaActivationStrategy extends ProtocolActivationStrategy {
               ),
             );
           } else {
+            if (KdfLoggingConfig.verboseLogging) {
+              _log.warning(
+                '[SIA] Activation failed for ${asset.id.id}: ${status.status} - ${status.details}',
+              );
+            }
+
             yield ActivationProgress(
               status: 'SIA activation failed',
               isComplete: true,
@@ -116,6 +141,10 @@ class SiaActivationStrategy extends ProtocolActivationStrategy {
         await Future<void>.delayed(kPollInterval);
       }
     } on Exception catch (e) {
+      if (KdfLoggingConfig.verboseLogging) {
+        _log.severe('[SIA] Activation exception for ${asset.id.id}: $e');
+      }
+
       yield ActivationProgress(
         status: 'SIA activation failed',
         isComplete: true,
@@ -129,4 +158,3 @@ class SiaActivationStrategy extends ProtocolActivationStrategy {
     }
   }
 }
-
